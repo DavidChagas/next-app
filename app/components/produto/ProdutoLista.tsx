91a04865f111091ee9433d04cc3aa040f5df34c6
DavidChagas <davidc@windowslive.com>
2023-05-19 14:28:12 -0300

    momentjs

--- app/components/produto/ProdutoLista.tsx
+++ app/components/produto/ProdutoLista.tsx
@@ -1,5 +1,6 @@
 import { Produto } from "@/types/Produto"
 import ProdutoItem from "./ProdutoItem"
+import moment from "moment";
 
 interface CategoriaProps {
     categoria: string;
@@ -7,22 +8,22 @@ interface CategoriaProps {
 
 export async function ProdutoLista({ categoria }: CategoriaProps){
 
-    let produtos:Array<Produto> = [];
-    
-    await fetch('https://fakestoreapi.com/products/category/'+ categoria)
-    .then(res=>res.json())
-    .then((data) =>{
-        produtos = data;
-        console.log(d)
-    })
-    .catch(error => {
-        console.error('Erro ao buscar produtos:', error);
-    });
-
-    
+  let produtos:Array<Produto> = [];
+  let formatteddatestr:any = '';
+  
+  await fetch('https://fakestoreapi.com/products/category/'+ categoria)
+  .then(res=>res.json())
+  .then((data) =>{
+      produtos = data;
+      formatteddatestr = moment( new Date() ).format('hh:mm:ss a');
+  })
+  .catch(error => {
+      console.error("Erro ao buscar produtos:", error);
+  });
 
   return (
     <>
+      <i>Data atual: {formatteddatestr}</i>
       <div className="grid grid-cols-5 gap-10">
         {produtos.map((produto) => (
           <ProdutoItem key={produto.id} produto={produto} />
