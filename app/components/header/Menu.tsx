71135c0f703925a32954b0e0a46500e6d917c558
DavidChagas <davidc@windowslive.com>
2023-06-07 09:17:33 -0300

    a

--- app/components/header/Menu.tsx
+++ app/components/header/Menu.tsx
@@ -2,20 +2,20 @@ import Link from 'next/link';
 import SearchProduto from './SearchProduto';
 
 export async function Menu() {
-  let categorias:string[] = [];
+  let categorias: string[] = [];
 
   const response = await fetch('https://fakestoreapi.com/products/categories');
   categorias = await response.json();
-  
-   return (
+
+  return (
     <div className='md:container md:mx-auto border-2 border-solid border-blue-300'>
       <div className="flex justify-center gap-10 m-10">
-        <Link href={'/'}>Home</Link>
+        <Link href={'/'}>HOME</Link>
         <Link href={'/produtos/todos'}>Todos os Produtos</Link>
         {categorias.map(item => (
-            <Link href={`/produtos/${item}`} key={item}>{item}</Link>
+          <Link href={`/produtos/${item}`} key={item}>{item}</Link>
         ))}
-        <SearchProduto/>
+        <SearchProduto />
       </div>
     </div>
   )
