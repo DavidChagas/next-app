69d2b63d81ec93b54996a1924d722170cba349a7
DavidChagas <davidc@windowslive.com>
2023-06-07 09:10:26 -0300

    "metadata" and "generateMetadata" cannot be exported at the same time

--- app/produtos/[categoria]/page.tsx
+++ app/produtos/[categoria]/page.tsx
@@ -1,11 +1,6 @@
 import { ProdutoLista } from "@/app/components/produto/ProdutoLista";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-    title: 'Produtos | {params.categoria}',
-    description: 'Produtos da Categoria ',
-};
-
 type Props = {
     params: { categoria: string };
 };
@@ -13,14 +8,14 @@ type Props = {
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
     const categoria = params.categoria;
     return {
-      title: 'Produtos | '+ categoria
+        title: 'Produtos | ' + categoria
     };
 }
 
-export default function ProdutosCategoriaPage({ params }: Props){
-    return(
+export default function ProdutosCategoriaPage({ params }: Props) {
+    return (
         <div className="md:container md:mx-auto px-10">
-            
+
             <h1 className="mt-10 mb-20 text-2xl">Produtos da Categoria {params.categoria}</h1>
             {/* @ts-expect-error Async Server Component */}
             <ProdutoLista categoria={params.categoria} />
